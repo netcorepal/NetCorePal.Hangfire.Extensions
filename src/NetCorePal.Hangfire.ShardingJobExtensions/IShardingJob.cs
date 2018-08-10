908360fdaf911f48b4c6b3f00d03a7a36ca43d59
weiyu.xiao <weiyu.xiao@xiaobao100.com>
2018-08-10 10:21:11 +0800

    添加项目

---
+++ src/NetCorePal.Hangfire.ShardingJobExtensions/IShardingJob.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCorePal.Hangfire.ShardingJobExtensions
+{
+    public interface IShardingJob
+    {
+        bool Execute(ShardingExecutionContext context);
+    }
+}
---
+++ src/NetCorePal.Hangfire.ShardingJobExtensions/ShardingExecutionContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCorePal.Hangfire.ShardingJobExtensions
+{
+    class ShardingExecutionContext
+    {
+        public int ShardingCount { get; set; }
+        public int ShardingIndex { get; set; }
+    }
+}
